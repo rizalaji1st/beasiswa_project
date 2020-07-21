07f073fe0fa1b2a0bb5b68cf2caa88d748628228
rendi10 <rendijaka@student.uns.ac.id>
2020-07-21 18:47:45 +0700

    pendaftaran

--- app/Http/Controllers/PendaftaranController.php
+++ app/Http/Controllers/PendaftaranController.php
@@ -16,53 +16,6 @@ class PendaftaranController extends Controller
         return view('pages.pendaftaran.home', ['beasiswas' => $beasiswas]);
     }
 
-<<<<<<< HEAD
-    public function article(){
-        return $this->belongsTo('App\Adminuniv');
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('pages.pendaftaran.create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Pendaftaran  $pendaftaran
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Pendaftaran $pendaftaran)
-    {
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Pendaftaran  $pendaftaran
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Pendaftaran $pendaftaran)
-    {
-        //
-    }
-=======
->>>>>>> 4f039c7b3216c1156f01aefa295900ddc35a36b1
 
     public function detail(Adminuniv $adminuniv)
     {
@@ -72,5 +25,20 @@ class PendaftaranController extends Controller
     public function daftar(Adminuniv $adminuniv)
     {
         return view('pages.pendaftaran.daftar', compact('adminuniv'));
+    }
+
+    public function tambah(Request $request)
+    {
+        $request->validate([
+            'id_penawaran' => 'required',
+            'id_pendaftar' => 'required',
+            'nim' => 'required',
+            'ips' => 'required|min:1|max:4',
+            'ipk' => 'required|min:1|max:4',
+            'semester' => 'required',
+            'penghasilan' => 'required',
+        ]);
+        Pendaftaran::create($request->all());
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -44,8 +44,6 @@ Route::get('/nrangking/{nrangking}', 'NrangkingsController@show');
 Route::post('/adminuniversitas', 'AdminunivController@store');
 
 Route::get('/', 'PendaftaranController@index');
-Route::get('/{adminuniv}', 'PendaftaranController@create');
-
 Route::get('/detail/{adminuniv}', 'PendaftaranController@detail');
 Route::get('/daftar/{adminuniv}', 'PendaftaranController@daftar');
-
+Route::post('/', 'PendaftaranController@tambah');
