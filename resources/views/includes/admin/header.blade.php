46121068c4439b18c11983016c48cb0472508335
ahnaffaiz <faiz.putra553@gmail.com>
2020-07-27 08:56:43 +0700

    add middleware

--- resources/views/includes/admin/header.blade.php
+++ resources/views/includes/admin/header.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
+    <link rel="icon" type="image/png" href="{{"frontend/img/uns.png"}}">
     <title>@yield('title')</title>
 
     @include('includes.admin.style')
