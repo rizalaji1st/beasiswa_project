3c084cc024df90b265a88b006785ed47b59e7415
ahnaffaiz <faiz.putra553@gmail.com>
2020-12-05 05:49:42 +0700

    ubah migrasi pendaftar penawaran ke integer

--- database/migrations/2020_02_19_061814_create_bea_pendaftar_penawaran.php
+++ database/migrations/2020_02_19_061814_create_bea_pendaftar_penawaran.php
@@ -14,11 +14,12 @@ class CreateBeaPendaftarPenawaran extends Migration
     public function up()
     {
         Schema::create('bea_pendaftar_penawaran', function (Blueprint $table) {
-            $table->bigInteger('id_pendaftar')->primary();
+            $table->bigIncrements('id_pendaftar');
             $table->unsignedBigInteger('id_penawaran');
             $table->foreign('id_penawaran')->references('id_penawaran')->on('bea_penawaran')->onDelete('cascade');
-            $table->integer('id_user');
-            $table->string('nim');
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('nim');
             $table->float('ips');
             $table->float('ipk');
             $table->integer('penghasilan');
@@ -29,8 +30,8 @@ class CreateBeaPendaftarPenawaran extends Migration
             $table->string('pendidikan_ibu'); //Tidak Sekolah, SD/MI / Sederajat, SMP/MTs / Sederajat, SMA/MA / Sederajat, D1 / Sederajat, D2 / Sederajat, D3 / Sederajat, D4/S1 / Sederajat, S2/Sp1 / Sederajat
             $table->string('pekerjaan_ayah');//TIDAK BEKERJA, Lainnya, Petani, Wirausaha, Peg. Swasta, PNS, TNI / POLRI
             $table->string('pekerjaan_ibu');//TIDAK BEKERJA, Lainnya, Petani, Wirausaha, Peg. Swasta, PNS, TNI / POLRI
-            $table->string('gaji_ayah');
-            $table->string('gaji_ibu');
+            $table->integer('gaji_ayah');
+            $table->integer('gaji_ibu');
             $table->integer('jumlah_tanggungan');
             $table->integer('semester');
             $table->boolean('is_finalisasi');
@@ -39,12 +40,15 @@ class CreateBeaPendaftarPenawaran extends Migration
             $table->string('finalized_at');
             $table->string('finalized_by');
             $table->string('printed_at');
-            $table->string('is_nominates');
-            $table->string('nominated_at');
-            $table->string('nominated_by');
-            $table->boolean('is_accepted');
-            $table->string('accepted_at');
-            $table->string('accepted_by');
+            $table->string('is_nominates')->nullable();
+            $table->string('nominated_at')->nullable();
+            $table->string('nominated_by')->nullable();
+            $table->boolean('is_accepted')->nullable();
+            $table->string('accepted_at')->nullable();
+            $table->string('accepted_by')->nullable();
+            $table->string('is_verified');//menunggu verifikasi, terverifikasi, ditodak, perbaikan 
+            $table->string('verified_at')->nullable();
+            $table->string('verified_by')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
