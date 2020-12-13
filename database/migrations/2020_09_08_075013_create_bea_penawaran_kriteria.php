46622eea795d6b29f343d6620bd067062d5df345
Luthfi Puji Ningtyas <luthfi_puji1@student.uns.ac.id>
2020-12-13 06:32:59 -0800

    aa

--- database/migrations/2020_09_08_075013_create_bea_penawaran_kriteria.php
+++ database/migrations/2020_09_08_075013_create_bea_penawaran_kriteria.php
@@ -20,7 +20,7 @@ class CreateBeaPenawaranKriteria extends Migration
             $table->string('nama_kriteria');
             $table->integer('bobot');
             $table->unsignedBigInteger('id_status');
-            $table->foreign('id_status')->references('id_status')->on('id_status')->onDelete('cascade');
+            $table->foreign('id_status')->references('id_status')->on('bea_status')->onDelete('cascade');
             $table->softDeletes();
             $table->timestamps();
         });
