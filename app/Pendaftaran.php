abe26a816e9687ee30fd7827ff4703a46a8ef4f4
ahnaffaiz <faiz.putra553@gmail.com>
2021-01-19 06:02:29 +0700

    memperbaiki fitur verifikasi

--- app/Pendaftaran.php
+++ app/Pendaftaran.php
@@ -10,13 +10,14 @@ class Pendaftaran extends Model
 {
     protected $table = 'bea_pendaftar_penawaran';
     protected $primaryKey = 'id_pendaftar';
-    protected $fillable = ['id_pendaftar', 
+    protected $fillable = [
                             'id_penawaran',
                             'id_user', 
                             'nim', 
                             'ips', 
                             'ipk', 
                             'penghasilan', 
+                            'semester', 
                             'status_ayah',
                             'status_ibu',
                             'status_rumah',
@@ -27,20 +28,32 @@ class Pendaftaran extends Model
                             'gaji_ayah',
                             'gaji_ibu',
                             'jumlah_tanggungan',
-                            'semester',
-                            'gaji_ayah',
-                            'gaji_ibu',
+                            'is_finalisasi',
                             'create_at',
                             'create_by',
                             'finalized_at',
                             'finalized_by',
-                            'is_verified'];
-    public function userPendaftar(){
-        return $this->belongsTo(User::class, 'id_user');
-    }
-
+                            'printed_at',
+                            'is_nominates',
+                            'nominated_at',
+                            'nominated_by',
+                            'is_accepted',
+                            'accepted_at',
+                            'accepted_by',
+                            'is_verified',
+                            'verified_at',
+                            'verified_by',
+                            'verified_note'
+                        ];
+                        
     public function pendaftaranUpload(){
-        return $this->hasMany(PendaftaranUpload::class,'id_pendaftar','id_pendaftar');
+        return $this->hasMany(filePendaftar::class, 'id_pendaftar', 'id_pendaftar');
+    }
+    public function pendaftarPenawaran(){
+        return $this->belongsTo(Penawaran::class);
+    }
+    public function userPendaftar(){
+        return $this->belongsTo(User::class,'id_user');
     }
 
     public function beastatus(){
