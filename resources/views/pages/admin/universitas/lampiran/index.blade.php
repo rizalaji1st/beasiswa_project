ddaf32be24fa03263a2c642fb51c5c11da869e78
ahnaffaiz <faiz.putra553@gmail.com>
2021-01-22 07:35:02 +0700

    fix datatables

--- resources/views/pages/admin/universitas/lampiran/index.blade.php
+++ resources/views/pages/admin/universitas/lampiran/index.blade.php
@@ -51,7 +51,7 @@
           </div>
           <div class="card-body">
             <div class="table-responsive">
-              <table id="beasiswa" class="table table-bordered table-hover" width="100%" cellspacing="0" style="border:1px solid #e3e6f0">
+              <table id="beasiswa" class="table table-bordered table-hover"  cellspacing="0" style="border:1px solid #e3e6f0;width=100%">
                 <thead>
                   <tr>
                     <th scope="col" class="text-center">No</th>
